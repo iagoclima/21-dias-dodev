f791274b7d85350650559983251d86895c1c7160
Iago Lima <iagoclima22@gmail.com>
2023-11-23 04:06:57 -0300

    exercício 2: Motorista

--- dia-4/index.js
+++ dia-4/index.js
@@ -1,11 +1,13 @@
-fome = prompt("Você está com fome? Digite S ou N:")
-dinheiro = prompt("Você tem dinheiro? Digite S ou N:")
-restaurante = prompt("O restaurante está aberto? Digite S ou N")
+nome = prompt("Digite seu nome:")
+idade = parseInt(prompt("Digite sua idade:"))
+carta = prompt("Possui carta de motorista? S ou N")
+carro = prompt("Possui carro? S ou N")
 
-if(fome == "N" || dinheiro == "N"){
-    console.log("Hoje a janta será em casa")
-} if(fome == "S" && dinheiro == "S" && restaurante == "N"){
-    console.log("Peça um delivery!")
-} else if(fome == "S" && dinheiro == "S" && restaurante == "S"){
-    console.log("Hoje o jantar será no seu restaurante preferido!")
+if(idade < 18 || carta == "N"){
+    console.log(nome + ", você não pode dirigir")
+}else if(idade >= 18 && carta == "S" || carro == "N"){
+    console.log(nome + ", você pode dirigir mas não tem um carro")
+}else if(idade >= 18 && carta == "S" || carro == "S"){
+    console.log(nome + ", você será o motorista")
 }
+
